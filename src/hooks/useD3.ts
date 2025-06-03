b56e09a74ff0e7ad9c60b64a59ee7a2a6a81bc34
Lombardo Paredes <lomba97@gmail.com>
2025-06-03 21:29:14 +0100

    "Add cleanup support to useD3 hook for improved chart lifecycle management"

--- src/hooks/useD3.ts
+++ src/hooks/useD3.ts
@@ -1,16 +1,22 @@
 import { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 
-export const useD3 = (renderChartFn: (svg: d3.Selection<SVGSVGElement, unknown, null, undefined>) => void, dependencies: React.DependencyList) => {
+export const useD3 = (renderChartFn: (svg: d3.Selection<SVGSVGElement, unknown, null, undefined>) => (() => void) | void, dependencies: React.DependencyList) => {
   const ref = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     const element = ref.current;
+    let cleanup: (() => void) | void;
+    
     if (element) {
       const svg = d3.select(element);
-      renderChartFn(svg);
+      cleanup = renderChartFn(svg);
     }
+    
     return () => {
+      if (cleanup) {
+        cleanup();
+      }
       if (element) {
         d3.select(element).selectAll('*').remove();
       }
